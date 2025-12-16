009f87cb2098706e22d5ea1a833a9d1ef2b79898
Hoyadong <as8645465@gmail.com>
2025-12-16 21:52:02 +0900

    feat: preview 추가

--- client/presentation/src/main/java/com/andone/memorip/presentation/HomeScreen.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/HomeScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.andone.memorip.presentation.theme.MemoripTheme
 
 @Composable
 fun HomeScreen() {
@@ -44,3 +46,10 @@ fun HomeScreenContents() {
     }
 }
 
+@Composable
+@Preview(showBackground = true)
+private fun HomeScreenContentsPreview(){
+    MemoripTheme {
+        HomeScreenContents()
+    }
+}
