e1ef2ecc1ff5582997b22cd40912c90a6643b93c
Hoyadong <as8645465@gmail.com>
2025-12-16 21:50:10 +0900

    feat: home topbar 추가

--- client/presentation/src/main/java/com/andone/memorip/presentation/HomeScreen.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/HomeScreen.kt
@@ -1,18 +1,46 @@
 package com.andone.memorip.presentation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 
 @Composable
-fun HomeScreen(){
+fun HomeScreen() {
     HomeScreenContents()
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreenContents(){
+fun HomeScreenContents() {
     Scaffold(
-
+        topBar = {
+            TopAppBar(
+                title = {},
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search_24),
+                            contentDescription = stringResource(R.string.search)
+                        )
+                    }
+                },
+                actions = {}
+            )
+        }
     ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier.padding(innerPadding)
+        ) {
 
+        }
     }
 }
+
