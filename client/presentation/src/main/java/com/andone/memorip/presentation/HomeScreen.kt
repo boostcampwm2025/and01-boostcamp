e1d94a6116aed3e7d61fae48c646a653b9104d02
Hoyadong <as8645465@gmail.com>
2025-12-16 21:54:19 +0900

    feat: top bar color 적용

--- client/presentation/src/main/java/com/andone/memorip/presentation/HomeScreen.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -34,7 +35,10 @@ fun HomeScreenContents() {
                         )
                     }
                 },
-                actions = {}
+                actions = {},
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MemoripTheme.colors.primaryContainer
+                )
             )
         }
     ) { innerPadding ->
