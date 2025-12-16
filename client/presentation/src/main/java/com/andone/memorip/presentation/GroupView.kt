52e5ef5a89bf0ad741eccf617d1a752640cf3b61
Hoyadong <as8645465@gmail.com>
2025-12-16 20:13:45 +0900

    feat: 그룹 뷰 컴포넌트 마무리

--- client/presentation/src/main/java/com/andone/memorip/presentation/GroupView.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/GroupView.kt
@@ -1,13 +1,16 @@
 package com.andone.memorip.presentation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.andone.memorip.domain.group.buildBento5x3Items
 import com.andone.memorip.presentation.theme.MemoripTheme
 
@@ -17,7 +20,10 @@ fun GroupView(
     images: List<String>,
     modifier: Modifier = Modifier
 ){
-    Column {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
         Text(name)
         GroupLayout(
             items = buildBento5x3Items(images),
