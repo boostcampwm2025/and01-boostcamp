c59bcda910689bb6f9a92af2780198eba5e8588e
HJunng <guswjd1gh@gmail.com>
2025-12-17 05:56:10 +0900

    feat: 앱 전반에서 사용될 Dimen 정의

---
+++ client/presentation/src/main/java/com/andone/memorip/presentation/theme/Dimen.kt
@@ -0,0 +1,10 @@
+package com.andone.memorip.presentation.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+object MemoripDimen {
+    val staggeredGridMinCellWidth: Dp = 160.dp
+    val staggeredGridSpacing: Dp = 4.dp
+    val imageCornerRadius: Dp = 4.dp
+}
