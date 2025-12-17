a7530b22ec03051e834037e4cc54baf34e97fa6b
gwondonghyeon <gwondh21@gmail.com>
2025-12-17 15:15:12 +0900

    feat: Dimen 추가

---
+++ client/presentation/src/main/java/com/andone/memorip/presentation/theme/Dimen.kt
@@ -0,0 +1,28 @@
+package com.andone.memorip.presentation.theme
+
+import androidx.compose.ui.unit.dp
+
+object MemoripPadding {
+    val PaddingXXXSmall = 2.dp
+    val PaddingXXSmall = 4.dp
+    val PaddingXSmall = 8.dp
+    val PaddingSmall = 12.dp
+    val PaddingMedium = 16.dp
+    val PaddingLarge = 20.dp
+    val PaddingXLarge = 24.dp
+}
+
+object MemoripSpace {
+    val SpaceXXSmall = 4.dp
+    val SpaceXSmall = 8.dp
+    val SpaceSmall = 12.dp
+    val SpaceMedium = 16.dp
+    val SpaceLarge = 20.dp
+    val SpaceXLarge = 24.dp
+}
+
+object MemoripIconSize {
+    val IconSizeSmall = 12.dp
+    val IconSizeMedium = 24.dp
+    val IconSizeLarge = 36.dp
+}
