14a8e78191d522690e2cda02af4ab7a3ba82ece2
uselessNaming <seohojon@naver.com>
2025-12-17 16:40:53 +0900

    feat: dummy category 클래스 추가

---
+++ client/presentation/src/main/java/com/andone/memorip/presentation/utils/dummy/DummyCategory.kt
@@ -0,0 +1,9 @@
+package com.andone.memorip.presentation.utils.dummy
+
+import androidx.compose.ui.graphics.Color
+
+data class DummyCategory(
+    val id: Long,
+    val category: String,
+    val color: Color
+)
