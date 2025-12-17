4b9099e630dd48b67a54386e1d843a73a0083e3e
uselessNaming <seohojon@naver.com>
2025-12-17 15:50:12 +0900

    feat: category item 컴포넌트 추가

---
+++ client/presentation/src/main/java/com/andone/memorip/presentation/component/CategoryItem.kt
@@ -0,0 +1,18 @@
+package com.andone.memorip.presentation.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.andone.memorip.presentation.theme.MemoripTheme
+
+@Composable
+fun CategoryItem() {
+
+}
+
+@Preview
+@Composable
+private fun CategoryItemPrev() {
+    MemoripTheme {
+        CategoryItem()
+    }
+}
