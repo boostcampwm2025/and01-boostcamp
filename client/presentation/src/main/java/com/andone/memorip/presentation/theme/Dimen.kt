0c9ad7c81f3739e8a53e9a732fe2bb203d7b49c4
gwondonghyeon <gwondh21@gmail.com>
2025-12-17 11:57:25 +0900

    feat: Dimen 추가

--- client/presentation/src/main/java/com/andone/memorip/presentation/theme/Dimen.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/theme/Dimen.kt
@@ -1,10 +1,27 @@
 package com.andone.memorip.presentation.theme
 
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-object MemoripDimen {
-    val staggeredGridMinCellWidth: Dp = 160.dp
-    val staggeredGridSpacing: Dp = 4.dp
-    val imageCornerRadius: Dp = 4.dp
+object MemoripPadding {
+    val PaddingXXSmall = 4.dp
+    val PaddingXSmall = 8.dp
+    val PaddingSmall = 12.dp
+    val PaddingMedium = 16.dp
+    val PaddingLarge = 20.dp
+    val PaddingXLarge = 24.dp
+}
+
+object MemoripSpace {
+    val SpaceXXSmall = 4.dp
+    val SpaceXSmall = 8.dp
+    val SpaceSmall = 12.dp
+    val SpaceMedium = 16.dp
+    val SpaceLarge = 20.dp
+    val SpaceXLarge = 24.dp
+}
+
+object MemoripIconSize {
+    val IconSizeSmall = 12.dp
+    val IconSizeMedium = 24.dp
+    val IconSizeLarge = 36.dp
 }
