279e62212a4216b470f027710a9234724d2daa35
uselessNaming <seohojon@naver.com>
2025-12-17 20:53:58 +0900

    feat: Select Category UI 추가

--- client/presentation/src/main/java/com/andone/memorip/presentation/selectcategory/SelectCategoryScreen.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/selectcategory/SelectCategoryScreen.kt
@@ -1,34 +1,126 @@
 package com.andone.memorip.presentation.selectcategory
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateSetOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.andone.memorip.presentation.R
+import com.andone.memorip.presentation.component.dialog.MemoripCategoryInputDialog
+import com.andone.memorip.presentation.selectcategory.component.CategoryItem
+import com.andone.memorip.presentation.selectcategory.model.Category
 import com.andone.memorip.presentation.theme.LocalMemoripColors
+import com.andone.memorip.presentation.theme.MemoripPadding.PaddingMedium
 import com.andone.memorip.presentation.theme.MemoripTheme
 import org.andone.memorip.presentation.theme.LocalMemoripTypography
 
 @Composable
 fun SelectCategoryScreen() {
+    val categories = remember{
+        mutableStateListOf(
+            Category(
+                id = 0L,
+                category = "맛집",
+                color = Color(0xFF000000)
+            ),
+            Category(
+                id = 1L,
+                category = "카페",
+                color = Color(0xFAA8F0F0)
+            ),
+            Category(
+                id = 2L,
+                category = "액티비티",
+                color = Color(0xFFCCDD66)
+            ),
+        )
+    }
+    val checkedSet = remember{ mutableStateSetOf<Long>() }
+    var showDialog by remember{ mutableStateOf(false) }
+
+    SelectCategoryContent(
+        categories = categories,
+        checkedList = checkedSet,
+        onShowDialog = { showDialog = true },
+        onCheckedChange = { id, checked ->
+            if (checked) {
+                if (checkedSet.size <= 3) checkedSet.add(id)
+//                else /** TODO snackbar로 3개까지만 담을 수 있다고 알려주기 */
+            } else {
+                checkedSet.remove(id)
+            }
+        }
+    )
+
+    if (showDialog) {
+        MemoripCategoryInputDialog(
+            title = stringResource(R.string.select_category_dialog_title),
+            onConfirmClick = { category, color ->
+                val newCategory = Category(
+                    id = (categories.maxOfOrNull { it.id } ?: -2L) + 1L,
+                    category =  category,
+                    color = color
+                )
+                categories.add(newCategory)
+                showDialog = false
+            },
+            onCancelClick = { showDialog = false },
+            onDismissRequest = { showDialog = false }
+        )
+    }
+}
+
+@Composable
+private fun SelectCategoryContent(
+    categories: List<Category>,
+    checkedList: Set<Long>,
+    onShowDialog: () -> Unit,
+    onCheckedChange: (id: Long, checked: Boolean) -> Unit
+) {
     Scaffold(
         topBar = { SelectCategoryTopBar() },
-        floatingActionButton = {}
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onShowDialog,
+            ) {
+                Icon(
+                    /** TODO 아이콘 변경해야 함 */
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_close),
+                    contentDescription = stringResource(R.string.select_category_create_category_button_description)
+                )
+            }
+        }
     ) { innerPadding ->
-        Column(
-            modifier = Modifier.padding(innerPadding)
+        LazyColumn(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(horizontal = PaddingMedium)
         ) {
-
+            items(categories) { category ->
+                CategoryItem(
+                    category = category,
+                    checked = category.id in checkedList,
+                    onCheckedChange = { onCheckedChange(category.id, it) }
+                )
+            }
         }
     }
 }
@@ -40,7 +132,7 @@ private fun SelectCategoryTopBar() {
         title = {
             Text(
                 text = stringResource(R.string.select_category_title),
-                style = LocalMemoripTypography.current.headline1
+                style = LocalMemoripTypography.current.headline2
             )
         },
         navigationIcon = {
