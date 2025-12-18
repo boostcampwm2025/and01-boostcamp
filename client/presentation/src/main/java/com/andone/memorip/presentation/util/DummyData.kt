cc9c6b21d975ce954ad441b00e223462ab0d1feb
HJunng <guswjd1gh@gmail.com>
2025-12-18 18:53:26 +0900

    feat: 장소들을 가져오는 사용될 더미 데이터 추가
    - `DummyData` 객체를 추가하여 앱의 여러 화면에서 사용할 임시 데이터를 제공
    - `Place`와 `ImageItem` 모델을 기반으로 더미 데이터를 생성
    - 이미지 URL은 `picsum.photos`를 활용하여 생성

---
+++ client/presentation/src/main/java/com/andone/memorip/presentation/util/DummyData.kt
@@ -0,0 +1,40 @@
+package com.andone.memorip.presentation.util
+
+import com.andone.memorip.presentation.model.ImageItem
+import com.andone.memorip.presentation.model.Place
+import kotlin.random.Random
+
+object DummyData {
+    private const val IMAGES_PER_PLACE = 50
+    
+    val places: List<Place> = listOf(
+        Triple(37.498095, 127.027610, "브런치 카페"),
+        Triple(37.512900, 127.058500, "예쁜 공원"),
+        Triple(37.517305, 127.047502, "야경 맛집"),
+        Triple(37.505228, 127.050324, "루프탑 바"),
+        Triple(37.508547, 127.062835, "숨은 카페"),
+        Triple(37.495592, 127.028747, "감성 서점")
+    ).mapIndexed { placeIndex, (lat, lng, placeName) ->
+        val images = List(IMAGES_PER_PLACE) { imageIndex ->
+            val photoId = Random.nextInt(30, 81)
+            val randomHeight = Random.nextInt(150, 400)
+            val fixedWidth = 200
+            
+            ImageItem(
+                id = placeIndex * IMAGES_PER_PLACE + imageIndex,
+                url = "https://picsum.photos/id/$photoId/$fixedWidth/$randomHeight",
+                width = fixedWidth,
+                height = randomHeight
+            )
+        }
+
+        Place(
+            id = placeIndex,
+            name = placeName,
+            latitude = lat,
+            longitude = lng,
+            thumbnailImage = images.first(),
+            images = images
+        )
+    }
+}
