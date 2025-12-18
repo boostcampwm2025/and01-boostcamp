75c606bcfd857a35a240b6027a939305bf0f6dc5
gwondonghyeon <gwondh21@gmail.com>
2025-12-18 16:13:24 +0900

    feat: Hilt 라이브러리 적용 및 Application 추가

--- client/presentation/build.gradle.kts
+++ client/presentation/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.jetbrains.kotlin.serialization)
+    alias(libs.plugins.dagger.hilt.root)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -56,6 +58,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation.compose)
+    ksp(libs.hilt.compiler)
 
     // Navigation
     implementation(libs.bundles.navigation3)
