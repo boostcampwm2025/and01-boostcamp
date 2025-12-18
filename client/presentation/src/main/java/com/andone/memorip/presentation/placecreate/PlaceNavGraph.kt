2ca8e5bd068871785be890a5d190dab3974c785a
hoyadong1 <8645465as@gmail.com>
2025-12-18 22:39:13 +0900

    chore: 코딩 컨벤션에 맞게 수정

--- client/presentation/src/main/java/com/andone/memorip/presentation/placecreate/PlaceNavGraph.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/placecreate/PlaceNavGraph.kt
@@ -5,14 +5,11 @@ import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import com.andone.memorip.navigation.PlaceCreate
 
-
 fun EntryProviderScope<NavKey>.placeCreate(
     onBackClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     entry<PlaceCreate> {
-        PlaceCreateScreen(
-            onBackClick = onBackClick
-        )
+        PlaceCreateScreen(onBackClick = onBackClick)
     }
 }
