52597855de1dc092c53e86997d2a281fc3889651
HJunng <guswjd1gh@gmail.com>
2025-12-18 19:31:37 +0900

    feat: GalleryTab이 Place 리스트를 받도록 수정

--- client/presentation/src/main/java/com/andone/memorip/presentation/groupdetail/component/GalleryTab.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/groupdetail/component/GalleryTab.kt
@@ -1,22 +1,25 @@
 package com.andone.memorip.presentation.groupdetail.component
 
-import android.content.res.Configuration
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.andone.memorip.presentation.component.MemoripStaggeredGrid
-import com.andone.memorip.presentation.groupdetail.model.PlaceImageItem
+import com.andone.memorip.presentation.model.Place
 import com.andone.memorip.presentation.theme.MemoripTheme
+import com.andone.memorip.presentation.util.DummyData
 
 @Composable
 fun GalleryTab(
-    images: List<PlaceImageItem>,
+    places: List<Place>,
     modifier: Modifier = Modifier
 ) {
+    val thumbnailImages = remember(places) {
+        places.map { it.thumbnailImage }
+    }
     MemoripStaggeredGrid(
-        images = images,
+        images = thumbnailImages,
         modifier = modifier
     )
 }
@@ -25,20 +28,8 @@ fun GalleryTab(
 @Composable
 private fun GalleryTabPreview() {
     MemoripTheme {
-        val dummyImages = remember {
-            List(30) { index ->
-                val randomHeight = (150..400).random()
-                val fixedWidth = 200
-                PlaceImageItem(
-                    id = index,
-                    url = "https://picsum.photos/id/${index + 1}/$fixedWidth/$randomHeight",
-                    width = fixedWidth,
-                    height = randomHeight
-                )
-            }
-        }
         GalleryTab(
-            images = dummyImages,
+            places = DummyData.places,
             modifier = Modifier.fillMaxSize()
         )
     }
