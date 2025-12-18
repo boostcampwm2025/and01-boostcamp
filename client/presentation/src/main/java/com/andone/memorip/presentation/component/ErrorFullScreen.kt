7b36c29ab1df43b84bff71fdc820272112277065
HJunng <guswjd1gh@gmail.com>
2025-12-18 19:11:27 +0900

    feat: 에러 화면 컴포저블 구현

---
+++ client/presentation/src/main/java/com/andone/memorip/presentation/component/ErrorFullScreen.kt
@@ -0,0 +1,71 @@
+package com.andone.memorip.presentation.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.andone.memorip.presentation.R
+import com.andone.memorip.presentation.theme.MemoripPadding
+import com.andone.memorip.presentation.theme.MemoripSpace
+import com.andone.memorip.presentation.theme.MemoripTheme
+
+@Composable
+fun ErrorFullScreen(
+    onRetry: () -> Unit,
+    modifier: Modifier = Modifier,
+    message: String = stringResource(R.string.errorfullscreen_error_default_message),
+    retryButtonText: String = stringResource(R.string.errorfullscreen_retry_button),
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(MemoripPadding.PaddingLarge),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = message,
+            color = MemoripTheme.colors.gray,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(MemoripSpace.SpaceLarge))
+
+        Button(onClick = onRetry) {
+            Text(retryButtonText)
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ErrorFullScreenPreview() {
+    MemoripTheme {
+        ErrorFullScreen(
+            onRetry = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ErrorFullScreenCustomPreview() {
+    MemoripTheme {
+        ErrorFullScreen(
+            onRetry = {},
+            message = "서버 연결에 실패했습니다\n잠시 후 다시 시도해주세요",
+            retryButtonText = "새로고침",
+        )
+    }
+}
