3ff3a96c8cce25738c3a2b53599d8c89a8931a03
uselessNaming <seohojon@naver.com>
2025-12-22 22:21:23 +0900

    feat: action 추가

---
+++ client/presentation/src/main/java/com/andone/memorip/presentation/selectgroup/model/SelectGroupAction.kt
@@ -0,0 +1,12 @@
+package com.andone.memorip.presentation.selectgroup.model
+
+import com.andone.memorip.presentation.home.model.GroupUiModel
+
+sealed interface SelectGroupAction {
+    data object onFABClick: SelectGroupAction
+    data class onGroupClick(val group: GroupUiModel): SelectGroupAction
+    data object onAddGroupClick: SelectGroupAction
+    data object onBackClick: SelectGroupAction
+    data class onConfirmDialogClick(val newGroup: GroupUiModel): SelectGroupAction
+    data object onDismissDialogClick: SelectGroupAction
+}
