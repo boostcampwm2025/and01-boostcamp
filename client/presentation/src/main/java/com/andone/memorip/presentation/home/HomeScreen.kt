c2de0738f74ad0a7ed97691fe37f41cd81ffacff
hoyadong1 <8645465as@gmail.com>
2025-12-22 15:16:13 +0900

    style: 홈화면 fab 버튼 컴포넌트로 분리

--- client/presentation/src/main/java/com/andone/memorip/presentation/home/HomeScreen.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/home/HomeScreen.kt
@@ -6,22 +6,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.andone.memorip.presentation.R
 import com.andone.memorip.presentation.component.GroupView
+import com.andone.memorip.presentation.home.component.HomeFloatingActionButton
 import com.andone.memorip.presentation.home.component.HomeTopBar
 import com.andone.memorip.presentation.home.model.GroupUiModel
 import com.andone.memorip.presentation.theme.MemoripPadding
@@ -62,18 +55,7 @@ fun HomeScreenContents(
     Scaffold(
         modifier = modifier,
         topBar = { HomeTopBar(onSearchClick = { }) },
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = onCreatePlaceClick,
-                containerColor = MemoripTheme.colors.primaryContainer,
-                contentColor = MemoripTheme.colors.black
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.ic_add),
-                    contentDescription = stringResource(R.string.home_add_content_description)
-                )
-            }
-        },
+        floatingActionButton = { HomeFloatingActionButton(onClick = onCreatePlaceClick) },
         contentWindowInsets = WindowInsets(),
     ) { innerPadding ->
         LazyColumn(
