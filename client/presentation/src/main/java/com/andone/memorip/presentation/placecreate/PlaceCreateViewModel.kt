ab9628dc7d6563b672cd6468ce67c7bc672fd609
gwondonghyeon <gwondh21@gmail.com>
2025-12-27 22:01:09 +0900

    feat: PlaceCreate ViewModel 추가

---
+++ client/presentation/src/main/java/com/andone/memorip/presentation/placecreate/PlaceCreateViewModel.kt
@@ -0,0 +1,45 @@
+package com.andone.memorip.presentation.placecreate
+
+import androidx.lifecycle.ViewModel
+import com.andone.memorip.presentation.placecreate.model.PlaceCreateAction
+import com.andone.memorip.presentation.placecreate.model.PlaceCreateEvent
+import com.andone.memorip.presentation.placecreate.model.PlaceCreateScreenState
+import com.andone.memorip.presentation.placecreate.model.PlaceCreateUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.BUFFERED
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class PlaceCreateViewModel @Inject constructor() : ViewModel() {
+
+    private val _uiState = MutableStateFlow(PlaceCreateUiState())
+    val uiState = _uiState.asStateFlow()
+
+    private val _event = Channel<PlaceCreateEvent>(capacity = BUFFERED)
+    val event = _event.receiveAsFlow()
+
+    fun onAction(action: PlaceCreateAction) {
+        when (action) {
+            PlaceCreateAction.OnCategoryClick -> {
+                _uiState.update { it.copy(screenState = PlaceCreateScreenState.SELECT_CATEGORY) }
+            }
+
+            PlaceCreateAction.OnLocationClick -> {
+                _uiState.update { it.copy(screenState = PlaceCreateScreenState.SELECT_LOCATION) }
+            }
+
+            PlaceCreateAction.OnGroupClick -> {
+                _uiState.update { it.copy(screenState = PlaceCreateScreenState.SELECT_GROUP) }
+            }
+
+            PlaceCreateAction.OnBackClick -> {
+                _event.trySend(PlaceCreateEvent.NavigateBack)
+            }
+        }
+    }
+}
