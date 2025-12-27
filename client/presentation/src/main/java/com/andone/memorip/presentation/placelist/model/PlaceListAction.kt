d10c891d397ceb354716c8aad079e248d9c09dd8
gwondonghyeon <gwondh21@gmail.com>
2025-12-27 21:31:11 +0900

    feat: PlaceList 관련 Action, Event 정의

--- client/presentation/src/main/java/com/andone/memorip/presentation/placelist/model/PlaceListAction.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/placelist/model/PlaceListAction.kt
@@ -2,9 +2,7 @@ package com.andone.memorip.presentation.placelist.model
 
 sealed interface PlaceListAction {
 
-    data class OnGroupAdd(val name: String) : PlaceListAction
+    data object OnFABClick : PlaceListAction
 
-    data class OnGroupUpdate(val group: GroupUiModel) : PlaceListAction
-
-    data class OnGroupsSet(val groups: List<GroupUiModel>) : PlaceListAction
+    data class OnGroupClick(val groupId: String) : PlaceListAction
 }
