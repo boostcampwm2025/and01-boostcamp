d077be309ec95359fb6f6820d7c563d733b4e4d2
gwondonghyeon <gwondh21@gmail.com>
2025-12-27 23:09:55 +0900

    feat: PlaceCreate Action 추가

--- client/presentation/src/main/java/com/andone/memorip/presentation/placecreate/model/PlaceCreateAction.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/placecreate/model/PlaceCreateAction.kt
@@ -1,5 +1,7 @@
 package com.andone.memorip.presentation.placecreate.model
 
+import android.net.Uri
+
 sealed interface PlaceCreateAction {
 
     data object OnCategoryClick : PlaceCreateAction
@@ -7,6 +9,14 @@ sealed interface PlaceCreateAction {
     data object OnLocationClick : PlaceCreateAction
 
     data object OnGroupClick : PlaceCreateAction
+
+    data class OnTitleChange(val title: String) : PlaceCreateAction
+
+    data class OnContentChange(val content: String) : PlaceCreateAction
+
+    data class OnAddImages(val images: List<Uri>) : PlaceCreateAction
+
+    data class OnRemoveImages(val imageUri: Uri) : PlaceCreateAction
 
     data object OnBackClick : PlaceCreateAction
 }
