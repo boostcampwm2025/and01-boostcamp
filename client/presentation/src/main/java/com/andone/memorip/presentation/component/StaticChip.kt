2a907664a5031b459493e1c4108edc5a0b385222
HJunng <guswjd1gh@gmail.com>
2025-12-29 22:44:17 +0900

    feat: StaticChip 컴포저블 구현
    - 고정된 형태의 태그를 표시하기 위한 `StaticChip` 컴포저블 구현.
    - `StaticChipColors` 데이터 클래스를 정의하여 칩의 배경, 텍스트, 테두리 색상을 변경할 수 있도록 구현.

---
+++ client/presentation/src/main/java/com/andone/memorip/presentation/component/StaticChip.kt
@@ -0,0 +1,94 @@
+package com.andone.memorip.presentation.component
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.andone.memorip.presentation.theme.MemoripBorderWidth
+import com.andone.memorip.presentation.theme.MemoripPadding
+import com.andone.memorip.presentation.theme.MemoripTheme
+
+private object StaticTagDimen {
+    val staticChipRaidus: Dp = 50.dp
+}
+
+@Immutable
+data class StaticChipColors(
+    val backgroundColor: Color,
+    val textColor: Color,
+    val borderColor: Color?
+) {
+    companion object {
+        val Default: StaticChipColors
+            @Composable
+            get() = StaticChipColors(
+                backgroundColor = MemoripTheme.colors.offWhite,
+                textColor = MemoripTheme.colors.onOffWhite,
+                borderColor = null
+            )
+    }
+}
+
+@Composable
+fun StaticChip(
+    chipName: String,
+    modifier: Modifier = Modifier,
+    radius: Dp = StaticTagDimen.staticChipRaidus,
+    colors: StaticChipColors = StaticChipColors.Default
+) {
+    val borderModifier = if (colors.borderColor != null) {
+        modifier.border(
+            width = MemoripBorderWidth.Thin,
+            color = colors.borderColor,
+            shape = RoundedCornerShape(radius)
+        )
+    } else { modifier }
+
+    Surface(
+        modifier = modifier.then(other = borderModifier),
+        shape = RoundedCornerShape(size = radius),
+        color = colors.backgroundColor,
+    ) {
+        Text(
+            text = chipName,
+            modifier = Modifier.padding(
+                horizontal = MemoripPadding.PaddingSmall,
+                vertical = MemoripPadding.PaddingXXSmall
+            ),
+            style = MemoripTheme.typography.label1,
+            color = colors.textColor
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StaticChipPreview() {
+    MemoripTheme {
+        Column {
+            StaticChip(chipName = "태그태그태그태그태그")
+            StaticChip(
+                chipName = "느좋",
+                colors = StaticChipColors.Default.copy(
+                    borderColor = MemoripTheme.colors.primary
+                )
+            )
+            StaticChip(
+                chipName = "카페",
+                radius = 10.dp,
+                colors = StaticChipColors.Default.copy(
+                    borderColor = MemoripTheme.colors.primary
+                )
+            )
+        }
+    }
+}
