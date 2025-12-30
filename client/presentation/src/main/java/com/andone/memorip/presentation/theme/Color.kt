76e44960a8f9864ba42ce26a880234d67ff77a1f
HJunng <guswjd1gh@gmail.com>
2025-12-30 15:39:40 +0900

    feat: 다크모드 색상 및 아이콘 크기 추가
    - 다크모드에서 사용될 색상 `Black`, `White`를 서로 교체
    - 아이콘 크기 `IconSizeSmall` 추가
    - 기존 `IconSizeSmall`은 `IconSizeXSmall`로 이름 변경

--- client/presentation/src/main/java/com/andone/memorip/presentation/theme/Color.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/theme/Color.kt
@@ -15,7 +15,7 @@ val Outline = Color(0xFF7A5A4A)
 val Gray = Color(0xFF808080)
 val Red = Color(0xFFB23A2E)
 
-val Black = Color(0xFF000000)
+val Black = Color(0xFF222222)
 val White = Color(0xFFFFFFFF)
 
 // Dark 테마용 색상
@@ -67,8 +67,8 @@ internal val darkMemoripColors = MemoripColors(
     gray = DarkGray,
     outline = DarkOutline,
     error = DarkRed,
-    black = Black,
-    white = White,
+    black = White,
+    white = Black,
 )
 
 internal val lightMaterialScheme = lightColorScheme(
