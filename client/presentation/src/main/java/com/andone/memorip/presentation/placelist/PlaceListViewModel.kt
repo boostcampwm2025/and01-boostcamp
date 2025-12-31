87f7aba36b2eee1a8f580ccea35c38918f4988e4
hoyadong1 <8645465as@gmail.com>
2025-12-31 15:27:57 +0900

    feat: 이벤트 변수 추가

--- client/presentation/src/main/java/com/andone/memorip/presentation/placelist/PlaceListViewModel.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/placelist/PlaceListViewModel.kt
@@ -1,8 +1,20 @@
 package com.andone.memorip.presentation.placelist
 
 import androidx.lifecycle.ViewModel
+import com.andone.memorip.presentation.placelist.model.PlaceListEvent
+import com.andone.memorip.presentation.placelist.model.PlaceListUiState
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.BUFFERED
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import javax.inject.Inject
 
 class PlaceListViewModel @Inject constructor() : ViewModel() {
 
+    private val _uiState = MutableStateFlow(value = PlaceListUiState())
+    val uiState = _uiState.asStateFlow()
+
+    private val _event = Channel<PlaceListEvent>(capacity = BUFFERED)
+    val event = _event.receiveAsFlow()
 }
