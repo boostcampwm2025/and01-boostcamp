da8efaba099722603e0a8dd1d8871430dc6b6bc2
hoyadong1 <8645465as@gmail.com>
2026-01-02 11:59:03 +0900

    chore: update git ignore

---
+++ server/src/main/kotlin/com/andone/memorip/common/config/NaverObjectStorageConfig.kt
@@ -0,0 +1,36 @@
+package com.andone.memorip.common.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.S3Configuration
+import java.net.URI
+
+@Configuration
+class NaverObjectStorageConfig {
+
+    @Bean
+    fun s3Client(
+        @Value("\${naver.s3.access-key}") accessKey: String,
+        @Value("\${naver.s3.secret-key}") secretKey: String,
+        @Value("\${naver.s3.endpoint}") endpoint: String,
+        @Value("\${naver.s3.region}") region: String
+    ): S3Client {
+        val credentials = AwsBasicCredentials.create(accessKey, secretKey)
+
+        return S3Client.builder()
+            .endpointOverride(URI.create(endpoint))
+            .region(Region.of(region))
+            .credentialsProvider(StaticCredentialsProvider.create(credentials))
+            .serviceConfiguration(
+                S3Configuration.builder()
+                    .pathStyleAccessEnabled(true)
+                    .build()
+            )
+            .build()
+    }
+}
