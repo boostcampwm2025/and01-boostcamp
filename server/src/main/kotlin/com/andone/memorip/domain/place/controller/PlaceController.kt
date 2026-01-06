d6a3ca0c8dfb9af59841ff7fa684b8e4c453923b
hoyadong1 <8645465as@gmail.com>
2026-01-06 18:54:57 +0900

    feat: 장소 리스트 기본 req body 추가

--- server/src/main/kotlin/com/andone/memorip/domain/place/controller/PlaceController.kt
+++ server/src/main/kotlin/com/andone/memorip/domain/place/controller/PlaceController.kt
@@ -4,6 +4,8 @@ import com.andone.memorip.common.response.ApiResult
 import com.andone.memorip.domain.place.dto.response.PlaceListItemResponse
 import com.andone.memorip.domain.place.service.PlaceService
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -15,7 +17,15 @@ class PlaceController(
 ) {
 
     @GetMapping
-    fun getPlaceList(pageable: Pageable): ApiResult<List<PlaceListItemResponse>> {
+    fun getPlaceList(
+        @PageableDefault(
+            page = 0,
+            size = 20,
+            sort = ["createdAt"],
+            direction = Sort.Direction.DESC
+        )
+        pageable: Pageable
+    ): ApiResult<List<PlaceListItemResponse>> {
         val result = placeService.getPlaceList(pageable)
         return ApiResult.success(result.content, result.pagination)
     }
