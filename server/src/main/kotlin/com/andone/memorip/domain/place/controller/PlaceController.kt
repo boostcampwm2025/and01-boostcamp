58f88a128d715821a51db6693fa763796564f055
hoyadong1 <8645465as@gmail.com>
2026-01-06 10:33:23 +0900

    feat: PlaceController 추가

--- server/src/main/kotlin/com/andone/memorip/domain/place/controller/PlaceController.kt
+++ server/src/main/kotlin/com/andone/memorip/domain/place/controller/PlaceController.kt
@@ -1,4 +1,24 @@
 package com.andone.memorip.domain.place.controller
 
-class PlaceController {
+import com.andone.memorip.common.response.ApiResult
+import com.andone.memorip.domain.place.dto.PlaceListItemResponse
+import com.andone.memorip.domain.place.service.PlaceService
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/places")
+class PlaceController(
+    private val placeQueryService: PlaceService
+) {
+
+    @GetMapping
+    fun getPlaces(
+        @PageableDefault(size = 10) pageable: Pageable
+    ): ApiResult<List<PlaceListItemResponse>> {
+        return placeQueryService.getPlaceList(pageable)
+    }
 }
