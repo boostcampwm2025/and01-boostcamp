48f0f7c94d0cddf3800b40b4155b649a663fd52a
HJunng <guswjd1gh@gmail.com>
2026-01-06 23:38:32 +0900

    feat: 커스텀 스낵바 MemoripSnackbar 구현
    - `MemoripApp`에서 사용하던 스낵바 로직을 `MemoripSnackbar` 컴포저블로 분리

--- client/presentation/src/main/java/com/andone/memorip/MemoripApp.kt
+++ client/presentation/src/main/java/com/andone/memorip/MemoripApp.kt
@@ -1,34 +1,18 @@
 package com.andone.memorip
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
-import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.andone.memorip.navigation.MemoripNav
 import com.andone.memorip.navigation.MemoripNavigator
 import com.andone.memorip.presentation.component.MainBottomBar
+import com.andone.memorip.presentation.component.MemoripSnackbar
 import com.andone.memorip.presentation.common.SnackBarManager
 import com.andone.memorip.presentation.snackbartest.SnackbarTestScreen
-import com.andone.memorip.presentation.theme.MemoripTheme
-
-private object SnackbarDimen {
-    val MAX_WIDTH = 400.dp
-    val MIN_HEIGHT = 80.dp
-    val HORIZONTAL_PADDING = 12.dp
-}
 
 @Composable
 fun MemoripApp(
@@ -60,25 +44,7 @@ fun MemoripApp(
             )
         },
         snackbarHost = {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = SnackbarDimen.HORIZONTAL_PADDING),
-                contentAlignment = Alignment.BottomCenter
-            ) {
-                SnackbarHost(hostState = snackbarHostState) { snackbarData ->
-                    Snackbar(
-                        snackbarData = snackbarData,
-                        modifier = Modifier
-                            .widthIn(max = SnackbarDimen.MAX_WIDTH)
-                            .heightIn(min = SnackbarDimen.MIN_HEIGHT),
-                        containerColor = MemoripTheme.colors.gray,
-                        contentColor = MemoripTheme.colors.white,
-                        actionColor = MemoripTheme.colors.primary,
-                        shape = MemoripTheme.shapes.roundedMedium
-                    )
-                }
-            }
+            MemoripSnackbar(hostState = snackbarHostState)
         },
         contentWindowInsets = WindowInsets()
     ) { innerPadding ->
