32966a4cb7b417452a9f2cadfcbbfa1d00ec0430
uselessNaming <seohojon@naver.com>
2026-01-07 02:04:15 +0900

    feat: ui state 변환 함수 추가

--- client/presentation/src/main/java/com/andone/memorip/presentation/placedetail/model/PlaceDetailUiState.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/placedetail/model/PlaceDetailUiState.kt
@@ -1,6 +1,19 @@
 package com.andone.memorip.presentation.placedetail.model
 
+import com.andone.memorip.domain.model.response.PlaceDetailResponse
+import kotlinx.collections.immutable.toImmutableList
+
 data class PlaceDetailUiState(
     val place: PlaceUiModel = PlaceUiModel(),
     val isLoading: Boolean = true
+)
+
+fun PlaceDetailResponse.toUiModel(): PlaceUiModel = PlaceUiModel(
+    id = this.id,
+    title = this.title,
+    tags = this.tags.toImmutableList(),
+    locationName = this.address.fullAddress,
+    imageUrls = this.images.toImmutableList(),
+    groupName = this.group.title,
+    content = this.content ?: "",
 )
