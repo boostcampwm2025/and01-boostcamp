6baea046b19590f5e98a8df2fd9db84b97cfa9c6
uselessNaming <seohojon@naver.com>
2026-01-07 02:02:02 +0900

    feat: category에서 tag로 변경

--- client/presentation/src/main/java/com/andone/memorip/presentation/placedetail/model/PlaceUiModel.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/placedetail/model/PlaceUiModel.kt
@@ -1,12 +1,13 @@
 package com.andone.memorip.presentation.placedetail.model
 
+import com.andone.memorip.domain.model.Tag
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 
 data class PlaceUiModel(
     val id: String = "",
     val title: String = "",
-    val category: String = "",
+    val tags: ImmutableList<Tag> = persistentListOf(),
     val locationName: String = "",
     val imageUrls: ImmutableList<String> = persistentListOf(),
     val groupName: String = "",
