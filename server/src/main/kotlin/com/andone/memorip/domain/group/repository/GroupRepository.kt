2898438ecfc10c7224c9bfb686ba058cbe8046e8
uselessNaming <seohojon@naver.com>
2026-01-07 02:07:44 +0900

    feat: group repository 추가

---
+++ server/src/main/kotlin/com/andone/memorip/domain/group/repository/GroupRepository.kt
@@ -0,0 +1,9 @@
+package com.andone.memorip.domain.group.repository
+
+import com.andone.memorip.domain.group.entity.Group
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface GroupRepository: JpaRepository<Group, UUID> {
+    fun findGroupById(id: UUID): Group?
+}
