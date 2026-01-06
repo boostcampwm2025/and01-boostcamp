2d525cf7735feea76f9b49ca96395e899a72d50a
hoyadong1 <8645465as@gmail.com>
2026-01-06 10:29:54 +0900

    feat: getPlaceList 함수 추가

--- server/src/main/kotlin/com/andone/memorip/domain/place/service/PlaceService.kt
+++ server/src/main/kotlin/com/andone/memorip/domain/place/service/PlaceService.kt
@@ -1,4 +1,42 @@
 package com.andone.memorip.domain.place.service
 
-class PlaceService {
+import com.andone.memorip.common.response.ApiResult
+import com.andone.memorip.domain.place.dto.PlaceListItemResponse
+import com.andone.memorip.domain.place.repository.PlaceRepository
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+import java.util.UUID
+
+@Service
+class PlaceService(
+    private val placeRepository: PlaceRepository
+) {
+    fun getPlaceList(
+        pageable: Pageable
+    ): ApiResult<List<PlaceListItemResponse>> {
+
+        val page = placeRepository.findAll(pageable)
+
+        val content = page.content.map { place ->
+            PlaceListItemResponse(
+                id = place.id!!,
+                title = place.title,
+                latitude = place.latitude,
+                longitude = place.longitude,
+                address = place.address.fullAddress,
+                startAt = place.startAt,
+                endAt = place.endAt,
+                imageUrl = place.getImages().firstOrNull()?.url
+            )
+        }
+
+        val pagination = ApiResult.PaginationInfo(
+            currentPage = page.number + 1,
+            totalPages = page.totalPages,
+            totalCount = page.totalElements,
+            hasNext = page.hasNext()
+        )
+
+        return ApiResult.success(content, pagination)
+    }
 }
