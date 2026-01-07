c4dd03374160396f4c0e557838928afd6ab122de
HJunng <guswjd1gh@gmail.com>
2026-01-07 21:56:21 +0900

    style: 네트워크 상태 모델 주석 제거

--- client/domain/src/main/java/com/andone/memorip/domain/model/NetworkStatus.kt
+++ client/domain/src/main/java/com/andone/memorip/domain/model/NetworkStatus.kt
@@ -1,28 +1,8 @@
 package com.andone.memorip.domain.model
 
-/**
- * 네트워크 연결 상태를 나타내는 sealed interface
- * 
- * 도메인 레벨의 네트워크 상태 추상화로, 플랫폼에 독립적입니다.
- */
 sealed interface NetworkStatus {
-    /**
-     * 네트워크가 정상적으로 연결된 상태
-     */
     data object Available : NetworkStatus
-
-    /**
-     * 네트워크 연결을 사용할 수 없는 상태
-     */
     data object Unavailable : NetworkStatus
-
-    /**
-     * 네트워크 신호가 약해지고 있는 상태
-     */
     data object Losing : NetworkStatus
-
-    /**
-     * 네트워크 연결이 완전히 끊어진 상태
-     */
     data object Lost : NetworkStatus
 }
