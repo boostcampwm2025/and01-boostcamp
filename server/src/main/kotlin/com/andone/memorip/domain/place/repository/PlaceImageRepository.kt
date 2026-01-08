8d2003bd2bd7f0219222d93ba7252852db7a0cab
uselessNaming <seohojon@naver.com>
2026-01-08 14:33:10 +0900

    fix: image, tag 받아오는 함수 수정

--- server/src/main/kotlin/com/andone/memorip/domain/place/repository/PlaceImageRepository.kt
+++ server/src/main/kotlin/com/andone/memorip/domain/place/repository/PlaceImageRepository.kt
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface PlaceImageRepository: JpaRepository<PlaceImage, UUID> {
+    fun findAllByPlaceId(id: UUID): List<PlaceImage>
 }
--- server/src/main/kotlin/com/andone/memorip/domain/place/repository/PlaceTagRepository.kt
+++ server/src/main/kotlin/com/andone/memorip/domain/place/repository/PlaceTagRepository.kt
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface PlaceTagRepository : JpaRepository<PlaceTag, UUID> {
+    fun findAllByPlaceId(id: UUID): List<PlaceTag>
 }
