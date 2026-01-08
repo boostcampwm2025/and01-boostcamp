84d44b69562cb21f925e0d4b406ae587600fad92
gwondonghyeon <gwondh21@gmail.com>
2026-01-09 02:26:57 +0900

    chore: Address 불필요한 함수 삭제

--- server/src/main/kotlin/com/andone/memorip/domain/place/entity/Address.kt
+++ server/src/main/kotlin/com/andone/memorip/domain/place/entity/Address.kt
@@ -35,20 +35,5 @@ class Address protected constructor() {
                 this.fullAddress = fullAddress
             }
         }
-
-        fun from(fullAddress: String): Address {
-            val parts = fullAddress.trim().split("\\s+".toRegex())
-
-            val depth1 = parts.getOrElse(0) { "" }
-            val depth2 = parts.getOrNull(1)
-            val depth3 = parts.getOrNull(2)
-
-            return Address(
-                region1Depth = depth1,
-                region2Depth = depth2,
-                region3Depth = depth3,
-                fullAddress = fullAddress
-            )
-        }
     }
 }
