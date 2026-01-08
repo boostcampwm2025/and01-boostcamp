e9937ae9458afa3e212573dadf6b1cc6103bfc8f
gwondonghyeon <gwondh21@gmail.com>
2026-01-09 02:28:39 +0900

    feat: PlaceCreateRequest data class의 UUID 타입을 String으로 수정

--- client/domain/src/main/java/com/andone/memorip/domain/model/request/PlaceCreateRequest.kt
+++ client/domain/src/main/java/com/andone/memorip/domain/model/request/PlaceCreateRequest.kt
@@ -1,18 +1,14 @@
 package com.andone.memorip.domain.model.request
 
-import com.andone.memorip.domain.util.UUIDSerializer
 import kotlinx.serialization.Serializable
-import java.util.UUID
 
 @Serializable
 data class PlaceCreateRequest(
-    @Serializable(with = UUIDSerializer::class)
-    val writerId: UUID,
+    val writerId: String,
+    val groupId: String,
     val title: String,
     val content: String? = null,
     val tag: List<String> = emptyList(),
-    @Serializable(with = UUIDSerializer::class)
-    val groupId: UUID,
     val latitude: Double,
     val longitude: Double,
     val address: Address,
