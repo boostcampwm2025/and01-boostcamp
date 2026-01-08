301c62300d29886b443623de66046bd3ec8b378b
gwondonghyeon <gwondh21@gmail.com>
2026-01-08 22:12:55 +0900

    chore: 오류 타입 및 메시지 수정

--- server/src/main/kotlin/com/andone/memorip/common/exception/ApiExceptionCode.kt
+++ server/src/main/kotlin/com/andone/memorip/common/exception/ApiExceptionCode.kt
@@ -22,5 +22,5 @@ enum class CommonExceptionCode(
     PLACE_NOT_FOUND(HttpStatus.NOT_FOUND, "장소를 찾을 수 없습니다."),
     GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "그룹을 찾을 수 없습니다."),
     INVALID_INVITE_CODE(HttpStatus.BAD_REQUEST, "잘못된 참여코드이거나 만료되었습니다."),
-    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "카테고리를 찾을 수 없습니다."),
+    TAG_NOT_FOUND(HttpStatus.NOT_FOUND, "태그를 찾을 수 없습니다."),
 }
