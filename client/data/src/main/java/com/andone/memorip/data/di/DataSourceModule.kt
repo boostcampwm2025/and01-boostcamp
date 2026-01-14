37a9e090a891c2fa7da3a641473e6b1257e0e754
HJunng <guswjd1gh@gmail.com>
2026-01-14 14:52:58 +0900

    fix: app과 연관된 group 관련 모듈 제거

--- client/data/src/main/java/com/andone/memorip/data/di/DataSourceModule.kt
+++ client/data/src/main/java/com/andone/memorip/data/di/DataSourceModule.kt
@@ -1,7 +1,5 @@
 package com.andone.memorip.data.di
 
-import com.andone.memorip.data.group.datasource.remote.GroupRemoteDataSource
-import com.andone.memorip.data.group.datasource.remote.GroupRemoteDataSourceImpl
 import com.andone.memorip.data.place.datasource.remote.PlaceRemoteDataSource
 import com.andone.memorip.data.place.datasource.remote.PlaceRemoteDataSourceImpl
 import dagger.Binds
@@ -17,8 +15,4 @@ abstract class DataSourceModule {
     @Binds
     @Singleton
     abstract fun bindPlaceDataSource(impl: PlaceRemoteDataSourceImpl): PlaceRemoteDataSource
-
-    @Binds
-    @Singleton
-    abstract fun bindGroupDataSource(impl: GroupRemoteDataSourceImpl): GroupRemoteDataSource
 }
