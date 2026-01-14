8db9824fd5440d3e02da54c6f01d3e42e8f14b15
HJunng <guswjd1gh@gmail.com>
2026-01-14 15:31:38 +0900

    feat: GroupUiModel 변환 로직 및 ID 타입 변경
    - `GroupUiModel`의 `id` 타입을 선택적(Optional)인 `UUID?`로 변경하고 기본값을 `null`로 설정
    - Domain 모델인 `Group`을 `GroupUiModel`로 변환하는 `from` 확장 함수 추가
    - `from` 함수에서 `group.title`을 `name`으로 매핑하고, `group.id`를 `UUID`로 변환하여 할당

--- client/presentation/src/main/java/com/andone/memorip/presentation/screen/grouplist/model/GroupUiModel.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/screen/grouplist/model/GroupUiModel.kt
@@ -1,14 +1,15 @@
 package com.andone.memorip.presentation.screen.grouplist.model
 
 import androidx.compose.runtime.Immutable
+import com.andone.memorip.domain.model.Group
 import java.util.UUID
 
 @Immutable
 data class GroupUiModel(
-    val id: UUID = UUID.randomUUID(),
+    val id: UUID? = null,
     val name: String,
     val images: List<String>,
-){
+) {
     companion object {
         fun default(): GroupUiModel {
             return GroupUiModel(
@@ -16,9 +17,18 @@ data class GroupUiModel(
                 images = emptyList()
             )
         }
+        
         fun create(name: String): GroupUiModel {
             return GroupUiModel(
                 name = name,
+                images = emptyList()
+            )
+        }
+
+        fun from(group: Group): GroupUiModel {
+            return GroupUiModel(
+                id = UUID.fromString(group.id),
+                name = group.title,
                 images = emptyList()
             )
         }
