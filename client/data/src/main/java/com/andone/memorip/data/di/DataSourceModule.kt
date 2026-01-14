3d839f6b796252444e4e837e71f9caa78a3a4252
HJunng <guswjd1gh@gmail.com>
2026-01-14 14:21:26 +0900

    feat: Group 관련 의존성 주입 설정 추가
    - `ServerNetworkModule`에 `GroupService` 제공 함수 추가
    - `DataSourceModule`에 `GroupRemoteDataSource` 바인딩 추가
    - `RepositoryModule`에 `GroupRepository` 바인딩 추가

--- client/data/src/main/java/com/andone/memorip/data/di/DataSourceModule.kt
+++ client/data/src/main/java/com/andone/memorip/data/di/DataSourceModule.kt
@@ -1,5 +1,7 @@
 package com.andone.memorip.data.di
 
+import com.andone.memorip.data.group.datasource.remote.GroupRemoteDataSource
+import com.andone.memorip.data.group.datasource.remote.GroupRemoteDataSourceImpl
 import com.andone.memorip.data.place.datasource.remote.PlaceRemoteDataSource
 import com.andone.memorip.data.place.datasource.remote.PlaceRemoteDataSourceImpl
 import dagger.Binds
@@ -15,4 +17,8 @@ abstract class DataSourceModule {
     @Binds
     @Singleton
     abstract fun bindPlaceDataSource(impl: PlaceRemoteDataSourceImpl): PlaceRemoteDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindGroupDataSource(impl: GroupRemoteDataSourceImpl): GroupRemoteDataSource
 }
--- client/data/src/main/java/com/andone/memorip/data/di/RepositoryModule.kt
+++ client/data/src/main/java/com/andone/memorip/data/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.andone.memorip.data.di
 
+import com.andone.memorip.data.group.repositoryimpl.GroupRepositoryImpl
 import com.andone.memorip.data.kakaosearch.repositoryimpl.KakaoSearchRepositoryImpl
 import com.andone.memorip.data.place.repositoryimpl.PlaceRepositoryImpl
+import com.andone.memorip.domain.repository.GroupRepository
 import com.andone.memorip.domain.repository.KakaoSearchRepository
 import com.andone.memorip.domain.repository.PlaceRepository
 import dagger.Binds
@@ -21,4 +23,8 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindPlaceRepository(impl: PlaceRepositoryImpl): PlaceRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindGroupRepository(impl: GroupRepositoryImpl): GroupRepository
 }
