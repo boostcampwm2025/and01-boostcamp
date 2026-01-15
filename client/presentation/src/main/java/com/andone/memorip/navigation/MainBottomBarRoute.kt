87d51b873f51241c0bc3fa7b2ba30f0797c520db
HJunng <guswjd1gh@gmail.com>
2026-01-16 00:40:28 +0900

    feat: 그룹 목록 화면 연결
    - 하단 바 메뉴에 `GROUP_LIST` 경로 활성화
    - `GroupListTopBar`의 배경색과 타이틀 색상 변경

--- client/presentation/src/main/java/com/andone/memorip/navigation/MainBottomBarRoute.kt
+++ client/presentation/src/main/java/com/andone/memorip/navigation/MainBottomBarRoute.kt
@@ -15,12 +15,11 @@ enum class MainBottomBarRoute(
         R.string.main_bottom_bar_place_list,
         PlaceList
     ),
-    // TODO: 백엔드 로직 구현 후 연결
-//    GROUP_LIST(
-//        R.drawable.ic_folder,
-//        R.string.main_bottom_bar_group,
-//        GroupList
-//    ),
+    GROUP_LIST(
+        R.drawable.ic_folder,
+        R.string.main_bottom_bar_group,
+        GroupList
+    ),
     USER(
         R.drawable.ic_account_circle,
         R.string.main_bottom_bar_user,
