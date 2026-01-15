5c3d68fd180e0f74119aacde2ff979eeccab57ea
hoyadong1 <8645465as@gmail.com>
2026-01-15 13:46:46 +0900

    style: bottom bar 디자인 수정

--- client/presentation/src/main/java/com/andone/memorip/presentation/component/MainBottomBar.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/component/MainBottomBar.kt
@@ -7,6 +7,7 @@ import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
@@ -31,6 +32,7 @@ import androidx.compose.ui.unit.dp
 import com.andone.memorip.navigation.MainBottomBarRoute
 import com.andone.memorip.presentation.R
 import com.andone.memorip.presentation.component.MainBottomBarConstants.DURATION_MILLIS
+import com.andone.memorip.presentation.component.MainBottomBarDimens.buttonOffset
 import com.andone.memorip.presentation.component.MainBottomBarDimens.centerButtonSize
 import com.andone.memorip.presentation.component.MainBottomBarDimens.elevation
 import com.andone.memorip.presentation.theme.MemoripTheme
@@ -38,8 +40,9 @@ import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 
 private object MainBottomBarDimens {
-    val centerButtonSize = 56.dp
+    val centerButtonSize = 52.dp
     val elevation = 8.dp
+    val buttonOffset = 10.dp
 }
 
 private object MainBottomBarConstants {
@@ -71,10 +74,13 @@ fun MainBottomBar(
             modifier = Modifier.fillMaxWidth(),
             contentAlignment = Alignment.BottomCenter
         ) {
-            NavigationBar(containerColor = MemoripTheme.colors.background) {
+            NavigationBar(
+                modifier = Modifier.height(height = 72.dp),
+                containerColor = MemoripTheme.colors.background
+            ) {
                 tabs.forEachIndexed { index, tab ->
                     if (index == tabs.size / 2) {
-                        Spacer(modifier = Modifier.width(centerButtonSize))
+                        Spacer(modifier = Modifier.width(width = centerButtonSize))
                     }
 
                     NavigationBarItem(
@@ -86,7 +92,7 @@ fun MainBottomBar(
                                 contentDescription = stringResource(tab.titleTextId)
                             )
                         },
-                        label = { Text(text = stringResource(tab.titleTextId)) },
+//                        label = { Text(text = stringResource(tab.titleTextId)) },
                         colors = NavigationBarItemDefaults.colors(
                             selectedIconColor = MaterialTheme.colorScheme.onSurface,
                             selectedTextColor = MaterialTheme.colorScheme.onSurface,
@@ -112,7 +118,7 @@ private fun BottomBarCenterButton(
         onClick = onClick,
         modifier = modifier
             .navigationBarsPadding()
-            .offset(y = -centerButtonSize)
+            .offset(y = -buttonOffset)
             .size(centerButtonSize)
             .shadow(elevation = elevation, shape = CircleShape),
         colors = IconButtonDefaults.iconButtonColors(containerColor = MemoripTheme.colors.primary)
