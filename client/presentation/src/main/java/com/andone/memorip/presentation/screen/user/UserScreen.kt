7ae0640feae3ed464d41140ed50fe41e900b4374
hoyadong1 <8645465as@gmail.com>
2026-01-15 22:10:15 +0900

    feat: 유저 로그인 상태 시각화 마무리

--- client/presentation/src/main/java/com/andone/memorip/presentation/screen/user/UserScreen.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/screen/user/UserScreen.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -18,6 +22,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,12 +38,14 @@ import com.andone.memorip.presentation.R
 import com.andone.memorip.presentation.component.MemoripImage
 import com.andone.memorip.presentation.screen.user.UserScreenConstants.ACCOUNT_SECTION_HEIGHT
 import com.andone.memorip.presentation.screen.user.UserScreenConstants.PLACES_SECTION_HEIGHT
+import com.andone.memorip.presentation.screen.user.UserScreenConstants.PROFILE_IMAGE_SIZE
 import com.andone.memorip.presentation.screen.user.model.UserAction
 import com.andone.memorip.presentation.screen.user.model.UserEvent
 import com.andone.memorip.presentation.screen.user.model.LoginMethod
 import com.andone.memorip.presentation.screen.user.model.UserUiModel
 import com.andone.memorip.presentation.screen.user.model.UserUiState
 import com.andone.memorip.presentation.theme.MemoripPadding
+import com.andone.memorip.presentation.theme.MemoripSpace
 import com.andone.memorip.presentation.theme.MemoripTheme
 import com.andone.memorip.presentation.theme.memoripShapes
 import com.andone.memorip.presentation.util.collectWithLifecycle
@@ -48,6 +56,7 @@ import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential.Co
 private object UserScreenConstants {
     val ACCOUNT_SECTION_HEIGHT = 180.dp
     val PLACES_SECTION_HEIGHT = 800.dp
+    val PROFILE_IMAGE_SIZE = 80.dp
 }
 
 @Composable
@@ -134,12 +143,28 @@ fun UserScreenContents(
             ) {
                 Box(contentAlignment = Alignment.Center) {
                     if (state.isLoggedIn) {
-                        Row {
-                            MemoripImage(
-                                imageUrl = state.user.profileImgUrl,
-                                contentDescription = stringResource(R.string.login_user_profile_image),
-                            )
-                            Text(text = state.user.name)
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Surface(
+                                modifier = Modifier
+                                    .size(PROFILE_IMAGE_SIZE)
+                                    .clip(memoripShapes.roundedXLarge)
+                            ) {
+                                val imageUrl = state.user.profileImgUrl
+
+                                if (imageUrl.isNotBlank()) {
+                                    MemoripImage(
+                                        imageUrl = imageUrl,
+                                        contentDescription = stringResource(R.string.login_user_profile_image),
+                                        modifier = Modifier.fillMaxSize(),
+                                        contentScale = ContentScale.Crop
+                                    )
+                                }
+                            }
+                            Spacer(modifier = Modifier.width(width = MemoripSpace.SpaceXXXLarge))
+                            Column {
+                                Text(text = state.user.id)
+                                Text(text = state.user.name)
+                            }
                         }
                     } else {
                         Text(text = stringResource(R.string.login_add_account))
