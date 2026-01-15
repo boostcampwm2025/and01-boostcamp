08a92056a28ed4e6352c98143929dcfae116545a
hoyadong1 <8645465as@gmail.com>
2026-01-15 17:14:08 +0900

    feat: userMe api 추가

---
+++ server/src/main/kotlin/com/andone/memorip/domain/user/controller/UserController.kt
@@ -0,0 +1,29 @@
+package com.andone.memorip.domain.user.controller
+
+import com.andone.memorip.common.response.ApiResult
+import com.andone.memorip.domain.user.dto.UserMeResponse
+import com.andone.memorip.domain.user.service.UserService
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/users")
+class UserController(
+    private val userService: UserService
+) {
+
+    @GetMapping("/me")
+    fun me(
+        @AuthenticationPrincipal firebaseUid: String
+    ): ApiResult<UserMeResponse> {
+
+        val user = userService.getOrCreateMe(
+            firebaseUid = firebaseUid,
+            nickname = null
+        )
+
+        return ApiResult.success(data = UserMeResponse.from(user))
+    }
+}
