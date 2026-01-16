f5abeed010223a6ad44a3de536c662961fba1391
gwondonghyeon <gwondh21@gmail.com>
2026-01-16 11:14:26 +0900

    chore: 버튼 색상 변경

--- client/presentation/src/main/java/com/andone/memorip/presentation/screen/selectlocation/component/LocationSelectionButton.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/screen/selectlocation/component/LocationSelectionButton.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.andone.memorip.presentation.R
 import com.andone.memorip.presentation.theme.MemoripShadow
 import com.andone.memorip.presentation.theme.MemoripTheme
@@ -28,7 +27,7 @@ fun LocationSelectionButton(
         modifier = modifier,
         enabled = enabled,
         colors = ButtonDefaults.buttonColors(
-            containerColor = MemoripTheme.colors.primaryContainer,
+            containerColor = MemoripTheme.colors.primary,
             contentColor = MaterialTheme.colorScheme.onSurface
         ),
         elevation = ButtonDefaults.buttonElevation(
