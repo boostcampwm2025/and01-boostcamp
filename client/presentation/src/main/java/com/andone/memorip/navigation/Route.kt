86f545e9c1411c5603b586836950a21b595e53b0
HJunng <guswjd1gh@gmail.com>
2026-01-16 10:24:23 +0900

    fix: GroupUiModel의 ID 타입을 String으로 변경
    - DummyData에서 `UUID.randomUUID()`를 `toString()`으로 변환하여 사용하도록 수정
    - `MemoripNavigator`의 `navigateToGroupDetail` 파라미터 타입을 `Int`에서 `String`으로 변경
    - `GroupDetail` 경로 클래스의 `groupId` 타입을 `Int`에서 `String`으로 변경

--- client/presentation/src/main/java/com/andone/memorip/navigation/Route.kt
+++ client/presentation/src/main/java/com/andone/memorip/navigation/Route.kt
@@ -19,7 +19,7 @@ data object Plan : NavKey
 data object PlaceCreate : NavKey
 
 @Serializable
-data class GroupDetail(val groupId: Int) : NavKey
+data class GroupDetail(val groupId: String) : NavKey
 
 @Serializable
 data class PlaceDetail(val placeId: String = "") : NavKey
