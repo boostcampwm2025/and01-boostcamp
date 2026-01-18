b828ce5085be264a9464a91e9c2a21d7b75189ee
gwondonghyeon <gwondh21@gmail.com>
2026-01-19 02:39:23 +0900

    chore: Action, Event 줄바꿈 적용

--- client/presentation/src/main/java/com/andone/memorip/presentation/screen/placedetail/model/PlaceDetailAction.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/screen/placedetail/model/PlaceDetailAction.kt
@@ -1,6 +1,8 @@
 package com.andone.memorip.presentation.screen.placedetail.model
 
 sealed interface PlaceDetailAction {
+
     data object OnBackClick : PlaceDetailAction
+
     data object OnAddToGroupClick : PlaceDetailAction
 }
--- client/presentation/src/main/java/com/andone/memorip/presentation/screen/placedetail/model/PlaceDetailEvent.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/screen/placedetail/model/PlaceDetailEvent.kt
@@ -1,7 +1,10 @@
 package com.andone.memorip.presentation.screen.placedetail.model
 
 sealed interface PlaceDetailEvent {
+
     data object NavigateBack : PlaceDetailEvent
+
     data object NavigateToSelectGroup : PlaceDetailEvent
+
     data object PlaceAddToGroup : PlaceDetailEvent
 }
