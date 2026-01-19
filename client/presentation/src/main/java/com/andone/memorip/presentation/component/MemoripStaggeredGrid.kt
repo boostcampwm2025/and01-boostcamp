0d3ac049ec347ec89428082891d23c6db99b0525
hoyadong1 <8645465as@gmail.com>
2026-01-19 12:45:07 +0900

    style: 장소 리스트 아이템 빈 주소 대응, 텍스트 간 간격 추가

--- client/presentation/src/main/java/com/andone/memorip/presentation/component/MemoripStaggeredGrid.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/component/MemoripStaggeredGrid.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,10 +18,9 @@ import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -28,16 +28,16 @@ import androidx.compose.ui.unit.dp
 import com.andone.memorip.presentation.component.StaggeredGridDimens.OVERLAY_HEIGHT
 import com.andone.memorip.presentation.component.StaggeredGridDimens.STAGGERED_GRID_IMAGE_CORNER_RADIUS
 import com.andone.memorip.presentation.model.Place
-import com.andone.memorip.presentation.theme.MemoripAlpha
 import com.andone.memorip.presentation.theme.MemoripPadding
 import com.andone.memorip.presentation.theme.MemoripSpace
 import com.andone.memorip.presentation.theme.MemoripTheme
 import com.andone.memorip.presentation.util.DummyData
+import com.andone.memorip.presentation.R
 
 private object StaggeredGridDimens {
     val STAGGERED_GRID_MIN_CELL_WIDTH = 160.dp
     val STAGGERED_GRID_IMAGE_CORNER_RADIUS = 16.dp
-    val OVERLAY_HEIGHT = 40.dp
+    val OVERLAY_HEIGHT = 52.dp
 }
 
 @Composable
@@ -102,18 +102,19 @@ fun StaggeredImageItem(
                 modifier = Modifier
                     .fillMaxHeight()
                     .padding(horizontal = MemoripPadding.PaddingXSmall),
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Center,
             ) {
-                if (contentDescription != null) {
-                    Text(
-                        text = contentDescription,
-                        style = MemoripTheme.typography.label1,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                }
-                if (!location.isNullOrBlank()) {
-                    PlaceLocationText(address = location)
-                }
+                Text(
+                    text = contentDescription?.takeIf { it.isNotBlank() }
+                        ?: stringResource(R.string.place_list_no_title),
+                    style = MemoripTheme.typography.label1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(modifier = Modifier.height(height = MemoripPadding.PaddingXXSmall))
+                PlaceLocationText(
+                    address = location?.takeIf { it.isNotBlank() }
+                        ?: stringResource(R.string.place_list_no_address_info)
+                )
             }
         }
     }
