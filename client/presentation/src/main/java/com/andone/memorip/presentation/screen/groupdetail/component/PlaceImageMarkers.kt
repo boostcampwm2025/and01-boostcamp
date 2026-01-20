ff78dc9429cc43a9aea2b1ed338c6ff35bad7875
HJunng <guswjd1gh@gmail.com>
2026-01-21 05:00:12 +0900

    feat: PlaceImageMarkers에 key 적용
    - 리컴포지션 최적화를 위해 `places` 루프 내부에 `key(place.id)` 추가

--- client/presentation/src/main/java/com/andone/memorip/presentation/screen/groupdetail/component/PlaceImageMarkers.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/screen/groupdetail/component/PlaceImageMarkers.kt
@@ -2,7 +2,8 @@ package com.andone.memorip.presentation.screen.groupdetail.component
 
 import android.graphics.Bitmap
 import androidx.compose.runtime.Composable
-import com.andone.memorip.presentation.component.ImageMarker
+import androidx.compose.runtime.key
+import com.andone.memorip.presentation.component.map.ImageMarker
 import com.andone.memorip.presentation.model.Place
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
@@ -17,21 +18,23 @@ fun PlaceImageMarkers(
     onMarkerClick: (Place) -> Unit
 ) {
     places.forEach { place ->
-        val imageUrl = place.thumbnailImage.url
-        val bitmap = markerImages[imageUrl]
+        key(place.id) {
+            val imageUrl = place.thumbnailImage.url
+            val bitmap = markerImages[imageUrl]
 
-        if (bitmap != null) {
-            MarkerComposable(
-                keys = arrayOf(place.id, imageUrl),
-                state = rememberUpdatedMarkerState(
-                    position = LatLng(place.latitude, place.longitude)
-                ),
-                onClick = {
-                    onMarkerClick(place)
-                    true
+            if (bitmap != null) {
+                MarkerComposable(
+                    keys = arrayOf(place.id, imageUrl),
+                    state = rememberUpdatedMarkerState(
+                        position = LatLng(place.latitude, place.longitude)
+                    ),
+                    onClick = {
+                        onMarkerClick(place)
+                        true
+                    }
+                ) {
+                    ImageMarker(imageBitmap = bitmap)
                 }
-            ) {
-                ImageMarker(imageBitmap = bitmap)
             }
         }
     }
