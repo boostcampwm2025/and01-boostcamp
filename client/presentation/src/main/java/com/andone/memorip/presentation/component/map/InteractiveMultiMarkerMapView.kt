10c2fa8867efb6acf9ebab3f86be1a8fe6abea2f
HJunng <guswjd1gh@gmail.com>
2026-01-21 05:13:24 +0900

    rename: InteractiveMultiMarkerMapView로 이름 변경
    - preview 추가

--- client/presentation/src/main/java/com/andone/memorip/presentation/component/map/InteractiveMultiMarkerMapView.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/component/map/InteractiveMultiMarkerMapView.kt
@@ -1,11 +1,14 @@
 package com.andone.memorip.presentation.component.map
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.andone.memorip.presentation.component.map.InteractiveMapViewDimen.BoundsPadding
+import com.andone.memorip.presentation.theme.MemoripTheme
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.geometry.LatLngBounds
 import com.naver.maps.map.CameraUpdate
@@ -20,11 +23,11 @@ private object InteractiveMapViewDimen {
 
 @OptIn(ExperimentalNaverMapApi::class)
 @Composable
-fun InteractiveMapView(
+fun InteractiveMultiMarkerMapView(
     initialBounds: List<LatLng>,
     modifier: Modifier = Modifier,
-    properties: MapProperties? = null,
-    uiSettings: MapUiSettings? = null,
+    properties: MapProperties = MemoripMapDefaults.defaultProperties,
+    uiSettings: MapUiSettings = MemoripMapDefaults.interactiveUiSettings,
     contentPadding: PaddingValues = PaddingValues(0.dp),
     onMapLoaded: () -> Unit = {},
     content: @Composable () -> Unit
@@ -44,11 +47,23 @@ fun InteractiveMapView(
     MemoripNaverMap(
         modifier = modifier,
         cameraPositionState = cameraPositionState,
-        properties = properties ?: MemoripMapDefaults.defaultProperties,
-        uiSettings = uiSettings ?: MemoripMapDefaults.interactiveUiSettings,
+        properties = properties,
+        uiSettings = uiSettings,
         contentPadding = contentPadding,
         onMapLoaded = onMapLoaded
     ) {
         content()
+    }
+}
+
+@Preview(name = "InteractiveMapView", showBackground = true)
+@Composable
+private fun InteractiveMapViewEmptyPreview() {
+    MemoripTheme {
+        InteractiveMultiMarkerMapView(
+            initialBounds = listOf(LatLng(37.5666805, 126.9784147)),
+            modifier = Modifier.fillMaxSize(),
+            content = {}
+        )
     }
 }
