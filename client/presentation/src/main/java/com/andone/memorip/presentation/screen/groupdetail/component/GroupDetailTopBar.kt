f5b7dea67f38aea508bcb69f141a09bc52400acb
HJunng <guswjd1gh@gmail.com>
2026-01-21 19:17:44 +0900

    fix: GroupDetailTopBar 색상 테마 적용
    - `TopAppBar`의 아이콘 및 타이틀 색상을 `MemoripTheme`의 `onSurface` 색으로 변경

--- client/presentation/src/main/java/com/andone/memorip/presentation/screen/groupdetail/component/GroupDetailTopBar.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/screen/groupdetail/component/GroupDetailTopBar.kt
@@ -4,7 +4,6 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -50,10 +49,9 @@ fun GroupDetailTopBar(
         },
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MemoripTheme.colors.background,
-            /** TODO 색상 변경하기 */
-            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-            navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-            actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+            navigationIconContentColor = MemoripTheme.colors.onSurface,
+            titleContentColor = MemoripTheme.colors.onSurface,
+            actionIconContentColor = MemoripTheme.colors.onSurface
         ),
         modifier = modifier
     )
