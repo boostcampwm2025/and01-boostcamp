7b743e9b2b01b53003759bbb729b01e23e1f5932
gwondonghyeon <gwondh21@gmail.com>
2026-01-22 22:46:30 +0900

    fix: ReadOnlyMapView 컴포넌트 파라미터 변경에 맞게 수정

--- client/presentation/src/main/java/com/andone/memorip/presentation/screen/placecreate/component/SelectRow.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/screen/placecreate/component/SelectRow.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -60,8 +61,8 @@ fun SelectRow(
     ) {
         location?.let {
             ReadOnlyMapView(
-                location = it,
-                aspectRatio = MAP_VIEW_ASPECT_RATIO
+                modifier = Modifier.aspectRatio(MAP_VIEW_ASPECT_RATIO),
+                location = it
             )
         }
 
