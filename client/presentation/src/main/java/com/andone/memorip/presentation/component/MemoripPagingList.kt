8abff0cb64966b69f42c5d7d9c19d10820629871
gwondonghyeon <gwondh21@gmail.com>
2026-01-26 00:07:06 +0900

    chore: MemoripPagingList 컴포넌트 컨벤션에 맞게 수정

--- client/presentation/src/main/java/com/andone/memorip/presentation/component/MemoripPagingList.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/component/MemoripPagingList.kt
@@ -22,10 +22,10 @@ import com.andone.memorip.presentation.util.handleAppendState
 fun <T : Any> MemoripPagingList(
     pagingItems: LazyPagingItems<T>,
     itemKey: (T) -> Any,
+    emptyContent: @Composable () -> Unit,
     modifier: Modifier = Modifier,
     staggeredCells: StaggeredGridCells? = null,
     initialContent: @Composable () -> Unit = {},
-    emptyContent: @Composable () -> Unit,
     itemContent: @Composable (T) -> Unit
 ) {
     val loadState = pagingItems.loadState
@@ -88,13 +88,12 @@ fun <T : Any> MemoripPagingList(
 
 @Preview(showBackground = true)
 @Composable
-fun MemoripPagingLocationsListPreview() {
+private fun MemoripPagingLocationsListPreview() {
     MemoripPagingList(
         pagingItems = DummyData.getLocationPagingItems(),
         itemKey = { it.id },
-        modifier = Modifier.fillMaxSize(),
-        initialContent = {},
         emptyContent = {},
+        modifier = Modifier.fillMaxSize(),
         itemContent = { location ->
             LocationItem(
                 location = location,
@@ -107,14 +106,13 @@ fun MemoripPagingLocationsListPreview() {
 
 @Preview(showBackground = true)
 @Composable
-fun MemoripPagingPlacesListPreview() {
+private fun MemoripPagingPlacesListPreview() {
     MemoripPagingList(
         pagingItems = DummyData.getPlacePagingItems(),
         itemKey = { it.id },
+        emptyContent = {},
         modifier = Modifier.fillMaxSize(),
         staggeredCells = StaggeredGridCells.Adaptive(160.dp),
-        initialContent = {},
-        emptyContent = {},
         itemContent = { place ->
             val image = place.thumbnailImage
             StaggeredImageItem(
