3514015807a8dc932692e479c87adacc29e70280
hoyadong1 <8645465as@gmail.com>
2026-01-26 16:59:24 +0900

    feat: 태그 조회 api 추가

--- server/src/main/kotlin/com/andone/memorip/domain/tag/controller/TagController.kt
+++ server/src/main/kotlin/com/andone/memorip/domain/tag/controller/TagController.kt
@@ -1,10 +1,14 @@
 package com.andone.memorip.domain.tag.controller
 
 import com.andone.memorip.common.response.ApiResult
+import com.andone.memorip.domain.place.dto.response.PlaceListItemResponse
 import com.andone.memorip.domain.tag.dto.request.TagCreateRequest
 import com.andone.memorip.domain.tag.dto.response.TagResponse
 import com.andone.memorip.domain.tag.dto.response.toTagCreateResponse
 import com.andone.memorip.domain.tag.service.TagService
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -19,5 +23,19 @@ class TagController(
     ): ApiResult<TagResponse>{
         val result = tagService.createTag(req)
         return ApiResult.success(result.toTagCreateResponse())
+    }
+
+    @GetMapping
+    fun getTagList(
+        @PageableDefault(
+            page = 0,
+            size = 20,
+            sort = ["createdAt"],
+            direction = Sort.Direction.DESC
+        )
+        pageable: Pageable
+    ): ApiResult<List<TagResponse>> {
+        val result = tagService.getTagList(pageable)
+        return ApiResult.success(result.content, result.pagination)
     }
 }
