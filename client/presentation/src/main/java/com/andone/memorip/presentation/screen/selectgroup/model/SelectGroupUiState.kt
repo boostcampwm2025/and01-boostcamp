ca946bfcfdaab44ca82701437ffd4acc6ffb4dd2
HJunng <guswjd1gh@gmail.com>
2026-01-27 02:08:01 +0900

    feat: 그룹 선택 화면 관련 Model 업데이트
    - `SelectGroupAction`에 `OnCheckClick` 액션 추가
    - `SelectGroupEvent`에 `PlaceGroupsUpdated` 이벤트 추가
    - `SelectGroupUiState`에 선택된 그룹 ID 관리를 위한 `selectedGroupIds`, `initialSelectedGroupIds` 상태 추가
    - `GroupUiModel`을 `SelectGroupUiModel`로 변경하여 그룹 선택 화면 전용 모델 사용하도록 수정

--- client/presentation/src/main/java/com/andone/memorip/presentation/screen/selectgroup/model/SelectGroupUiState.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/screen/selectgroup/model/SelectGroupUiState.kt
@@ -1,10 +1,13 @@
 package com.andone.memorip.presentation.screen.selectgroup.model
 
-import com.andone.memorip.presentation.screen.grouplist.model.GroupUiModel
 import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.ImmutableSet
+import kotlinx.collections.immutable.persistentSetOf
 import kotlinx.collections.immutable.toImmutableList
 
 data class SelectGroupUiState(
-    val groups: ImmutableList<GroupUiModel> = emptyList<GroupUiModel>().toImmutableList(),
+    val groups: ImmutableList<SelectGroupUiModel> = emptyList<SelectGroupUiModel>().toImmutableList(),
+    val selectedGroupIds: ImmutableSet<String> = persistentSetOf(),
+    val initialSelectedGroupIds: ImmutableSet<String> = persistentSetOf(),
     val isLoading: Boolean = false
 )
