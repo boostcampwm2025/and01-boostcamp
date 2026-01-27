2a54047b59fe1a496c55757272342b26febc8021
uselessNaming <seohojon@naver.com>
2026-01-27 21:56:04 +0900

    feat: Action, Event 추가

--- client/presentation/src/main/java/com/andone/memorip/presentation/screen/plan/model/PlanAction.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/screen/plan/model/PlanAction.kt
@@ -1,5 +1,6 @@
 package com.andone.memorip.presentation.screen.plan.model
 
+import com.andone.memorip.presentation.model.GroupUiModel
 import com.andone.memorip.presentation.model.Place
 import java.time.LocalDate
 
@@ -23,4 +24,6 @@ sealed interface PlanAction {
     data class DateSelected(val start: LocalDate, val end: LocalDate) : PlanAction
 
     data class DayScrolled(val day: Int) : PlanAction
+    data object GroupChoiceClick : PlanAction
+    data class GroupChoiceConfirmClick(val selectedGroup: GroupUiModel) : PlanAction
 }
--- client/presentation/src/main/java/com/andone/memorip/presentation/screen/plan/model/PlanEvent.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/screen/plan/model/PlanEvent.kt
@@ -2,4 +2,5 @@ package com.andone.memorip.presentation.screen.plan.model
 
 sealed interface PlanEvent {
     data class ShowDeleteDayDialog(val day: Int?) : PlanEvent
+    data object ShowGroupChoiceDialog: PlanEvent
 }
