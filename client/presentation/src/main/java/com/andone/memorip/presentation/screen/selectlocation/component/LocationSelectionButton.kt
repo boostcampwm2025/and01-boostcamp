2263207cb4ba24e1a3b312b63e3bfb3d4185fe4a
gwondonghyeon <gwondh21@gmail.com>
2026-01-27 23:06:30 +0900

    feat: LocationSelectionButton 색상 수정

--- client/presentation/src/main/java/com/andone/memorip/presentation/screen/selectlocation/component/LocationSelectionButton.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/screen/selectlocation/component/LocationSelectionButton.kt
@@ -1,6 +1,7 @@
 package com.andone.memorip.presentation.screen.selectlocation.component
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -28,7 +29,8 @@ fun LocationSelectionButton(
         enabled = enabled,
         colors = ButtonDefaults.buttonColors(
             containerColor = MemoripTheme.colors.primary,
-            contentColor = MemoripTheme.colors.onSurface
+            contentColor = MemoripTheme.colors.onSurface,
+            disabledContainerColor = MemoripTheme.colors.primaryContainer
         ),
         elevation = ButtonDefaults.buttonElevation(
             defaultElevation = MemoripShadow.Medium,
@@ -51,11 +53,27 @@ fun LocationSelectionButton(
 @Preview
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-private fun LocationSelectionButtonPreview() {
+private fun LocationSelectionButtonEnableTruePreview() {
     MemoripTheme {
-        LocationSelectionButton(
-            onClick = {},
-            enabled = true
-        )
+        Column {
+            LocationSelectionButton(
+                enabled = true,
+                onClick = {}
+            )
+        }
+    }
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun LocationSelectionButtonEnableFalsePreview() {
+    MemoripTheme {
+        Column {
+            LocationSelectionButton(
+                enabled = false,
+                onClick = {}
+            )
+        }
     }
 }
