dedc00e360ef8f80a6718037b7b74b9eea2f6c4f
HJunng <guswjd1gh@gmail.com>
2026-01-30 06:45:31 +0900

    refactor: Group 관련 네이밍을 Trip으로 변경

---
+++ client/domain/src/main/java/com/andone/memorip/domain/repository/TripRepository.kt
@@ -0,0 +1,38 @@
+package com.andone.memorip.domain.repository
+
+import androidx.paging.PagingData
+import com.andone.memorip.domain.model.Group
+import com.andone.memorip.domain.model.TripListItem
+import com.andone.memorip.domain.model.TripPlace
+import com.andone.memorip.domain.model.TripWithPlaceAdded
+import com.andone.memorip.domain.model.PlaceListItem
+import com.andone.memorip.domain.model.Visibility
+import kotlinx.coroutines.flow.Flow
+
+interface TripRepository {
+    val myTrips: Flow<List<Group>>
+    suspend fun fetchMyTrips(page: Int = 0, size: Int = 20): Result<Unit>
+    suspend fun fetchMyTripsWithPlaceStatus(
+        page: Int = 0,
+        size: Int = 20,
+        placeId: String?
+    ): Result<List<TripWithPlaceAdded>>
+
+    suspend fun getPublicTrips(page: Int = 0, size: Int = 20): Result<List<Group>>
+    suspend fun getTripById(tripId: String): Result<Group>
+    suspend fun createTrip(ownerId: String, title: String, visibility: Visibility): Result<Group>
+    suspend fun updateTrip(
+        tripId: String,
+        title: String,
+        visibility: Visibility,
+        startDate: String?,
+        endDate: String?
+    ): Result<Unit>
+
+    suspend fun deleteTrip(tripId: String): Result<Unit>
+    suspend fun addPlaceToTrip(tripId: String, placeId: String): Result<Unit>
+    fun getTripPlaces(tripId: String): Flow<PagingData<PlaceListItem>>
+    suspend fun getSimpleTrips(): Result<List<TripListItem>>
+    suspend fun getPlaceByTripId(tripId: String): Result<List<TripPlace>>
+    suspend fun updatePlaceTime(tripPlaceId: String, startAt: String, endAt: String): Result<Unit>
+}
