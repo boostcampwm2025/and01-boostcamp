c384445088210f59addd9cdd29165399e68997bc
HJunng <guswjd1gh@gmail.com>
2026-01-29 12:28:16 +0900

    feat: 장소 요청 및 응답 DTO에 Swagger Schema 및 필드 개선 적용
    - PlaceRequest DTO에 @Schema 애노테이션 추가 및 필드 설명, 예제 값 정의
    - 태그 필드를 tags로 수정 및 ID 리스트로 데이터 타입 변경
    - 그룹 ID 필드에 @Schema 애노테이션 추가
    - PlaceDetailResponse DTO에 groups 필드 리스트 타입으로 변경

--- server/src/main/kotlin/com/andone/memorip/feature/place/dto/response/PlaceDetailResponse.kt
+++ server/src/main/kotlin/com/andone/memorip/feature/place/dto/response/PlaceDetailResponse.kt
@@ -1,8 +1,8 @@
 package com.andone.memorip.feature.place.dto.response
 
-import com.andone.memorip.feature.group.dto.response.GroupResponse
 import com.andone.memorip.feature.place.entity.Address
 import com.andone.memorip.feature.place.entity.PlaceTag
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.UUID
 
 data class PlaceDetailResponse(
@@ -14,8 +14,10 @@ data class PlaceDetailResponse(
     val content: String?,
     val latitude: Double,
     val longitude: Double,
-    val group: GroupResponse,
-    val address: Address
+    val groups: List<GroupSimpleResponse>,
+    val address: Address,
+    @get:JsonProperty("isMine")
+    val isMine: Boolean
 )
 
 data class TagResponse(
