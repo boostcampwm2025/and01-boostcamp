5f893c3152570cad26b18c128d8d762d824ea499
uselessNaming <seohojon@naver.com>
2026-02-03 21:47:14 +0900

    feat: 삭제 이미지 추가

--- client/presentation/src/main/java/com/andone/memorip/presentation/screen/plan/component/TimeBlockItem.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/screen/plan/component/TimeBlockItem.kt
@@ -1,44 +1,41 @@
 package com.andone.memorip.presentation.screen.plan.component
 
-import android.util.Log
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.DraggableState
-import androidx.compose.foundation.gestures.awaitDragOrCancellation
-import androidx.compose.foundation.gestures.awaitEachGesture
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.awaitLongPressOrCancellation
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.gestures.drag
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import com.andone.memorip.domain.model.TimeBlock
+import com.andone.memorip.presentation.R
 import com.andone.memorip.presentation.screen.plan.component.TimeBlockItemConstants.DELETE_THRESHOLD
 import com.andone.memorip.presentation.screen.plan.component.TimeBlockItemConstants.SNAP_MINUTE_UNIT
 import com.andone.memorip.presentation.screen.plan.utill.MINUTE_HEIGHT_DP
@@ -46,6 +43,7 @@ import com.andone.memorip.presentation.screen.plan.utill.TimeLayoutEngine
 import com.andone.memorip.presentation.theme.MemoripDragConstants.DRAG_SCALE
 import com.andone.memorip.presentation.theme.MemoripDragConstants.DRAG_SHADOW_ELEVATION
 import com.andone.memorip.presentation.theme.MemoripDragConstants.DRAG_Z_INDEX
+import com.andone.memorip.presentation.theme.MemoripIconSize
 import com.andone.memorip.presentation.theme.MemoripPadding
 import com.andone.memorip.presentation.theme.MemoripTheme
 import com.andone.memorip.presentation.theme.memoripShapes
@@ -77,7 +75,7 @@ fun TimeBlockItem(
         modifier = modifier
             .offset {
                 IntOffset(
-                    x = dragOffsetX.roundToInt(),
+                    x = 0,
                     y = (startYPx + dragOffsetY).roundToInt()
                 )
             }
@@ -100,8 +98,36 @@ fun TimeBlockItem(
             )
     ) {
         Box(
+            modifier = Modifier.fillMaxHeight()
+                .width(dragOffsetX.coerceAtLeast(0f).dp)
+                .background(
+                    color = MemoripTheme.colors.error,
+                    shape = MemoripTheme.shapes.roundedMedium
+                )
+                .padding(all = MemoripPadding.PaddingMedium)
+                .graphicsLayer{
+                    scaleX = 0.8f + (dragOffsetX / DELETE_THRESHOLD).coerceIn(0f, 1f) * 0.2f
+                    scaleY = 0.8f + (dragOffsetX / DELETE_THRESHOLD).coerceIn(0f, 1f) * 0.2f
+                },
+            contentAlignment = Alignment.CenterStart
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_outline_delete_24),
+                contentDescription = stringResource(R.string.plan_delete),
+                modifier = Modifier.size(size = MemoripIconSize.IconSizeLarge),
+                tint = MemoripTheme.colors.white
+            )
+        }
+
+        Box(
             modifier = Modifier
                 .fillMaxSize()
+                .offset {
+                    IntOffset(
+                        x = dragOffsetX.roundToInt(),
+                        y = 0
+                    )
+                }
                 .pointerInput(startYPx) {
                     detectDragGesturesAfterLongPress(
                         onDragStart = {
@@ -150,7 +176,6 @@ fun TimeBlockItem(
             content()
         }
     }
-
 }
 
 @Preview(showBackground = true)
