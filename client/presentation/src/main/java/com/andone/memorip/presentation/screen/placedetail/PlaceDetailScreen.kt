95f498d372dc8c503305c8967a52d071c1ff01f2
uselessNaming <seohojon@naver.com>
2026-02-04 02:33:28 +0900

    fix: 화면 버벅거림 제거

--- client/presentation/src/main/java/com/andone/memorip/presentation/screen/placedetail/PlaceDetailScreen.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/screen/placedetail/PlaceDetailScreen.kt
@@ -15,18 +15,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -43,6 +41,7 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.fontscaling.MathUtils.lerp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
@@ -54,9 +53,11 @@ import com.andone.memorip.presentation.component.dialog.DeleteDialog
 import com.andone.memorip.presentation.model.LocationUiModel
 import com.andone.memorip.presentation.screen.placedetail.PlaceDetailScreenConstants.BOTTOM_ALPHA
 import com.andone.memorip.presentation.screen.placedetail.PlaceDetailScreenConstants.BOTTOM_RATIO
+import com.andone.memorip.presentation.screen.placedetail.PlaceDetailScreenConstants.MAX_HEIGHT_RATE
 import com.andone.memorip.presentation.screen.placedetail.PlaceDetailScreenConstants.MIDDLE_ALPHA
 import com.andone.memorip.presentation.screen.placedetail.PlaceDetailScreenConstants.MIDDLE_RATIO
 import com.andone.memorip.presentation.screen.placedetail.PlaceDetailScreenConstants.MIN_HEIGHT_RATE
+import com.andone.memorip.presentation.screen.placedetail.PlaceDetailScreenConstants.SCROLL_SPEED
 import com.andone.memorip.presentation.screen.placedetail.PlaceDetailScreenConstants.TOP_ALPHA
 import com.andone.memorip.presentation.screen.placedetail.PlaceDetailScreenConstants.TOP_RATIO
 import com.andone.memorip.presentation.screen.placedetail.PlaceDetailScreenDimens.OVERLAY_HEIGHT
@@ -78,12 +79,14 @@ import com.andone.memorip.presentation.util.toPx
 
 private object PlaceDetailScreenConstants {
     const val MIN_HEIGHT_RATE = 0.5f
+    const val MAX_HEIGHT_RATE = 0.5f
     const val TOP_ALPHA = 0f
     const val MIDDLE_ALPHA = 0.8f
     const val BOTTOM_ALPHA = 1f
     const val TOP_RATIO = 0f
     const val MIDDLE_RATIO = 0.5f
     const val BOTTOM_RATIO = 1f
+    const val SCROLL_SPEED = 1000f
 }
 
 private object PlaceDetailScreenDimens {
@@ -207,20 +210,10 @@ private fun PlaceDetailContent(
 ) {
     val density = LocalDensity.current
     val pagerState = rememberPagerState(pageCount = { place.imageUrls.size })
-    val scrollState = rememberScrollState()
     val statusBarHeightDp = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
     val maxHeaderHeight = LocalWindowInfo.current.containerDpSize.height - statusBarHeightDp
-    val minHeaderHeight = maxHeaderHeight * MIN_HEIGHT_RATE
     val maxHeaderPx = maxHeaderHeight.toPx(density = density)
-    val minHeaderPx = minHeaderHeight.toPx(density = density)
-    val collapseRangePx = maxHeaderPx - minHeaderPx
-    val headerHeightPx by remember {
-        derivedStateOf {
-            val collapseOffset = scrollState.value.toFloat()
-                .coerceIn(0f, collapseRangePx)
-            maxHeaderPx - collapseOffset
-        }
-    }
+    val lazyListState = rememberLazyListState()
 
     CompositionLocalProvider(
         LocalOverscrollFactory provides null
@@ -243,6 +236,7 @@ private fun PlaceDetailContent(
             contentWindowInsets = WindowInsets.navigationBars
         ) { innerPadding ->
             LazyColumn(
+                state = lazyListState,
                 modifier = modifier
                     .fillMaxSize()
                     .padding(bottom = innerPadding.calculateBottomPadding())
@@ -250,10 +244,18 @@ private fun PlaceDetailContent(
                 verticalArrangement = Arrangement.spacedBy(space = MemoripSpace.SpaceXXXLarge)
             ) {
                 item {
+                    val scrollOffset = lazyListState.firstVisibleItemScrollOffset.toFloat()
+
+                    val heightFraction = lerp(
+                        start = MAX_HEIGHT_RATE,
+                        stop = MIN_HEIGHT_RATE,
+                        amount = (scrollOffset / SCROLL_SPEED).coerceIn(0f, 1f)
+                    )
+
                     Box(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .height(height = headerHeightPx.toDp(density = density))
+                            .height(height = (maxHeaderPx * heightFraction).toDp(density))
                             .clipToBounds()
                             .clickable { onImageClick(place.imageUrls[pagerState.currentPage]) },
                         contentAlignment = Alignment.BottomStart
