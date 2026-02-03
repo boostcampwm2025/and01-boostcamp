b17c7ced6e822aad2561e23dee3156dfa89d5c23
uselessNaming <seohojon@naver.com>
2026-02-03 15:09:12 +0900

    chore: 컨벤션 정리

--- client/presentation/src/main/java/com/andone/memorip/presentation/screen/plan/PlanViewModel.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/screen/plan/PlanViewModel.kt
@@ -1,6 +1,5 @@
 package com.andone.memorip.presentation.screen.plan
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.work.BackoffPolicy
