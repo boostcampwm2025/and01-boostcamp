d4f2b82350ebacc96e314e5c52a62cb037e620f6
uselessNaming <seohojon@naver.com>
2026-02-04 10:21:41 +0900

    chore: 버전 코드 및 이름 변경

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.andone.memorip"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.4.0"
+        versionCode = 8
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
