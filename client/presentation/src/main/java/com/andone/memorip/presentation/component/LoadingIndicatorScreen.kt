e9d0e7beafc6fa6d63e1a1cfe579434f024bb9f1
gwondonghyeon <gwondh21@gmail.com>
2026-02-05 21:31:02 +0900

    chore: 파라미터 줄 정리

--- client/presentation/src/main/java/com/andone/memorip/presentation/component/LoadingIndicatorScreen.kt
+++ client/presentation/src/main/java/com/andone/memorip/presentation/component/LoadingIndicatorScreen.kt
@@ -12,7 +12,8 @@ import androidx.compose.ui.tooling.preview.Preview
 @Composable
 fun LoadingIndicatorScreen(modifier: Modifier = Modifier) {
     Box(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
+            .fillMaxSize()
             .clickable(enabled = true, onClick = {}),
         contentAlignment = Alignment.Center
     ) {
